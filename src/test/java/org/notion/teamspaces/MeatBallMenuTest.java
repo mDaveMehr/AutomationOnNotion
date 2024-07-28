0bf5564cbc3d0a89476cb217af9bc422a664a602
= <=>
2024-07-28 12:38:17 -0400

    implementing copy link functionality

--- src/test/java/org/notion/teamspaces/MeatBallMenuTest.java
+++ src/test/java/org/notion/teamspaces/MeatBallMenuTest.java
@@ -5,23 +5,42 @@ import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utils.TestRunPropertyReader;
 
 public class MeatBallMenuTest extends BaseTest {
     protected MeatballMenu meatballMenu;
     @BeforeMethod
     public void initializeCreateTaskPage() {
         meatballMenu = PageFactory.initElements(driver, MeatballMenu.class);
-        meatballMenu.openMeatballMenu();
     }
-
     @Test(priority = 1)
-    public void verifyLockDatabase(){
+    public void verifyToggleLockDatabase() {
+        meatballMenu.openMeatballMenu();
         pauseBrowser(3);
-        if(meatballMenu.lockDatabaseBtn.isDisplayed()){
-            Assert.assertEquals(meatballMenu.getLockDatabaseStatusText(), "Locked");
-        }else{
-            Assert.assertNull(meatballMenu.getLockDatabaseStatusText());
-        }
-    }
+        meatballMenu.lockStatusText.getText(); // "Unlocked"
 
+        boolean result = meatballMenu.toggleLockDatabase();
+        if(!result){
+            Assert.assertEquals(meatballMenu.getLockDatabaseStatusText(),
+                    TestRunPropertyReader.getTestRunProperty("lockDatabaseStatus"), "Database status should be 'Locked' after clicking the button.");
+        }else{
+            Assert.assertTrue(result, "Method should return true when the database is unlocked.");
+        }
+
+    }
+    @Test(priority = 2)
+    public void verifyCopyLinkOption(){
+        pauseBrowser(3);
+        meatballMenu.copyLink();
+        Assert.assertEquals(meatballMenu.getClipboardText(),
+                TestRunPropertyReader.getTestRunProperty("clipboardText"));
+    }
+    @Test(priority = 3)
+    public void verifyMoveToOption(){
+        meatballMenu.openMeatballMenu();
+        meatballMenu.moveToLink();
+        Assert.assertEquals(meatballMenu.getMessageAfterMovingPage(),
+                TestRunPropertyReader.getTestRunProperty("movedTasksToPrivate"));
+    }
 }
